fafbd62412e0f30029a2e5612007bf6238b36bae
joaquin683 <joaquin@guudcompany.cl>
2026-05-11 12:39:22 -0300

    Init Posthog in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { initPosthog } from '../lib/analytics'
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    initPosthog()
+  }, [])
   return <Component {...pageProps} />
 }
