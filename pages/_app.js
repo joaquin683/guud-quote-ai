6a8e9d121b60ad7ac7fbab41d7f76a3962789b77
joaquin683 <joaquin@guudcompany.cl>
2026-06-03 14:58:57 -0300

    feat: favicon GUUD global en _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import '../styles/globals.css'
 import { useEffect } from 'react'
+import Head from 'next/head'
 import { initPosthog } from '../lib/analytics'
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     initPosthog()
   }, [])
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/Favicon.png" />
+        <link rel="shortcut icon" href="/Favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
