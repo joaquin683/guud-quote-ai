f98884f930b6fb8b44a1bae3ec418f8257c234f9
joaquin683 <joaquin@guudcompany.cl>
2026-06-03 15:54:47 -0300

    fix: favicon SVG inline (U neon) sin archivo externo

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import { useEffect } from 'react'
 import Head from 'next/head'
 import { initPosthog } from '../lib/analytics'
 
+const FAVICON = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Crect width='100' height='100' rx='20' fill='%230A0A0A'/%3E%3Ctext x='50' y='73' font-family='Arial,Helvetica,sans-serif' font-size='76' font-weight='700' fill='%23C9FF00' text-anchor='middle'%3E%C3%9C%3C/text%3E%3C/svg%3E"
+
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     initPosthog()
@@ -10,8 +12,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" type="image/png" href="/Favicon.png" />
-        <link rel="shortcut icon" href="/Favicon.png" />
+        <link rel="icon" href={FAVICON} />
       </Head>
       <Component {...pageProps} />
     </>
